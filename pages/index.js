5c4fcfc1bd8fa008fbe85edd8359e148a6141d05
Michael Yang <chengzhou.y@gmail.com>
2021-11-06 21:32:49 +0000

    Reformat index

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,11 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-
-const YEAR = new Date().getFullYear()
+import Container from "../components/Container";
 
 export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Hey, my name is Michael and I'm a Data Scientist. 
-          I'm currently learning about web development, and will be building this personal website where I 
-          eventually plan on posting about stuff I'm working on and things I enjoy. More to come soon...
-        </p>
-        <p>
-          In the meantime, you can find me on{' '}
-          <a href="https://linkedin.com/in/chengzhouyang">LinkedIn</a> or {' '}
-          <a href="https://github.com/mch-yang">GitHub</a>.
-        </p>
-      </section>
-      <section className={utilStyles.footer}>
-        <p>
-          <time>{YEAR}</time> © Michael Yang
-        </p>
-      </section>
-    </Layout>
-  )
+    <Container
+      title="Michael Yang"
+      description="My personal website"
+    >
+    </Container>
+  );
 }
