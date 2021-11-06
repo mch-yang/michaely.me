db3f2ce306e68665e2c7e4f4b35fed695855640b
Michael Yang <chengzhou.y@gmail.com>
2021-11-06 21:31:17 +0000

    Add container, navbar and footer components

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+const YEAR = new Date().getFullYear()
+
+export default function Footer() {
+  return (
+    <div className="bg-[#F1F1F1] dark:bg-gray-900">
+      <div className="max-w-6xl  mx-auto px-4 py-10 md:py-20">
+        <div className="h-0.5 w-full bg-white dark:bg-gray-700"></div>
+        <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
+          <div>
+            <p>&copy; {YEAR} Michael Yang.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
