a1cd9f4fd52868b7923734921011ad73e9219420
Michael Yang <chengzhou.y@gmail.com>
2021-11-13 22:04:25 +0000

    Fix footer darkmode background

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ const YEAR = new Date().getFullYear()
 
 export default function Footer() {
   return (
-    <div className="bg-white fixed bottom-0 pt-4 pb-4 dark:bg-gray-800">
+    <div className="bg-white w-screen md:w-auto fixed bottom-0 pt-4 pb-4 dark:bg-gray-800">
             <p className="text-center text-xs">
               &copy; {YEAR} Michael Yang
             </p>
