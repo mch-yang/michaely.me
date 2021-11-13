b5622aee4a44cd2d3be5b0055454722e528ff8ad
Michael Yang <chengzhou.y@gmail.com>
2021-11-13 18:01:34 +0000

    Center resume items on small screen

--- components/ResumeItem.js
+++ components/ResumeItem.js
@@ -1,15 +1,17 @@
 export default function ResumeItem({company, text, year}) {
   return (
-      <div className="max-w-6xl mx-auto mb-8 items-center px-20">
+      <div className="max-w-6xl mx-auto mb-8 items-center py-6">
         <div className="flex flex-col justify-between items-center md:flex-row">
-          <h4 className="flex mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
+          <h4 className="flex mb-2 text-lg font-medium text-gray-900 dark:text-gray-100">
             {company}
           </h4>
-          <p className="w-32 mb-4 text-left text-gray-500 md:text-right md:mb-0">
+          <p className="w-32 mb-4 text-gray-500 md:text-right text-center md:mb-0">
             {year}
           </p>
         </div>
-        <p className="text-gray-600 dark:text-gray-400">{text}</p>
+        <p className="text-gray-600 dark:text-gray-400 md:text-left text-center">
+          {text}
+        </p>
       </div>
   )
 }
--- pages/resume.js
+++ pages/resume.js
@@ -7,29 +7,30 @@ export default function Resume() {
       title="Resume – Michael Yang"
       description="Experience"
     >
-      <div className="max-w-6xl mx-auto w-auto mb-8 px-20">
-          <h4 className="flex mb-2 text-xl font-medium text-gray-900 md:text-xl dark:text-gray-100">
-            Work Experience
-          </h4>
-      </div>
 
-      <ResumeItem
-        company="Amazon.com, Inc."
-        year="2019 – present"
-        text="Data Scientist II"
-      />
+    <div className="max-w-6xl mx-auto w-auto mb-8 px-20">
+        <h4 className="text-xl font-medium md:text-left text-center text-gray-900 dark:text-gray-100">
+          Work Experience
+        </h4>
 
-      <div className="max-w-6xl mx-auto w-auto mb-8 px-20">
-          <h4 className="flex mb-2 text-xl font-medium text-gray-900 md:text-xl dark:text-gray-100">
-            Education
-          </h4>
-      </div>
+        <ResumeItem
+          company="Amazon.com, Inc."
+          year="2019 – present"
+          text="Data Scientist II"
+        />
+    </div>
 
-      <ResumeItem
-        company="University of Oxford"
-        year="2017 – 2018"
-        text="MSc in Statistical Science"
-      />
+    <div className="max-w-6xl mx-auto w-auto mb-8 px-20">
+        <h4 className="text-xl font-medium md:text-left text-center text-gray-900 dark:text-gray-100">
+          Education
+        </h4>
+
+        <ResumeItem
+          company="University of Oxford"
+          year="2017 – 2018"
+          text="MSc in Statistical Science"
+        />
+    </div>
 
     </Container>
   );
