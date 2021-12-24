8f959e5870579690043f8b98dee9e8c31f495e4b
Michael Yang <chengzhou.y@gmail.com>
2021-12-24 13:48:16 +0100

    Improve music layout

--- pages/music.js
+++ pages/music.js
@@ -1,10 +1,32 @@
+import Link from 'next/link'
+
 import Container from '../components/Container';
 import TopTracks from '../components/TopTracks';
 
 export default function Music() {
   return (
     <Container>
-      <TopTracks/>
+      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto pb-16">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+          Music
+        </h1>
+        <h2 className="font-bold text-3xl tracking-tight mb-4 mt-8 text-black dark:text-white">
+          Top Songs
+        </h2>
+        <div className="mb-[-1.4rem]">
+          <p className="text-gray-600 dark:text-gray-400 mb-4">
+            These are my most played songs on Spotify at the moment. {' '}
+            <Link href="/contact">
+              <a className="text-gray-900 dark:text-gray-100 underline">
+                Get in touch
+              </a>
+            </Link>
+            {' '}
+            if you have any recommendations!
+          </p>
+        </div>
+        <TopTracks/>
+      </div>
     </Container>
   )
 }
