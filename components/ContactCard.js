256ac9e28620957538b06a546982fbfd0567dbf4
Michael Yang <chengzhou.y@gmail.com>
2021-12-25 14:19:18 +0100

    Add contact card to contact page

---
+++ components/ContactCard.js
@@ -0,0 +1,33 @@
+export default function ContactCard() {
+  return (
+    <div className="flex flex-col w-96 pt-2 mt-2">
+      <div className="items-left m-4 border rounded-lg border-gray-200 dark:border-zinc-700 bg-gray-100 dark:bg-zinc-800 hover:shadow">
+        <div className="flex flex-row m-4">
+          <div className="flex flex-col m-2"> 
+            <p className="text-gray-800 dark:text-gray-200 font-medium">
+              Name
+            </p>
+            <p className="text-gray-800 dark:text-gray-200 font-medium">
+              Email
+            </p>
+            <p className="text-gray-800 dark:text-gray-200 font-medium">
+              LinkedIn
+            </p>
+          </div>
+
+          <div className="flex flex-col m-2"> 
+            <p>
+              Michael Yang
+            </p>
+            <a href="mailto:michael@michaely.me" className="hover:text-blue-500">
+              michael@michaely.me
+            </a>
+            <a href="https://linkedin.com/in/mchyang" className="hover:text-blue-500" target="_blank">
+              /in/mchyang
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,18 @@
 import Container from "../components/Container";
+import ContactCard from "../components/ContactCard";
 
 export default function Contact() {
   return (
     <Container>
+      <div className="flex flex-col grow justify-center max-w-2xl mx-auto pb-16">
+        <h1 className="font-bold text-3xl sm:text-5xl tracking-tight mb-4 text-black dark:text-white">
+          Contact
+        </h1>
+        <p className="text-gray-600 dark:text-gray-400 mb-4">
+          Feel free to get in touch with me using one of the methods below – I'm always up for a chat! {' '}
+        </p>
+        <ContactCard />
+      </div>
     </Container>
   );
 }
