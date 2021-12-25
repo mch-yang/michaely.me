a9665865502916a8cdb32b25bb6e0fef644828d2
Michael Yang <chengzhou.y@gmail.com>
2021-12-25 12:28:24 +0100

    Bugfix

--- pages/api/playlist.js
+++ pages/api/playlist.js
@@ -3,8 +3,8 @@ import { getPlaylist } from '../../lib/spotify';
 const playlist_id = process.env.PLAYLIST_ID_CURRENT_FAVOURITES;
 
 export default async function handler(req, res) {
-  const response = await getPlaylist();
-  const { items } = await response.json(playlist_id);
+  const response = await getPlaylist(playlist_id);
+  const { items } = await response.json();
 
   const tracks = items.map((item) => ({
     artist: item.track.artists.map((_artist) => _artist.name).join(', '),
