5e959927bf638d7228e0add3f219763173e21574
Michael Yang <chengzhou.y@gmail.com>
2021-12-25 21:02:01 +0100

    Improve contact card spacing

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -1,7 +1,7 @@
 export default function ContactCard() {
   return (
-    <div className="flex flex-col w-96 pt-2 mt-2">
-      <div className="items-left m-4 border rounded-lg border-gray-200 dark:border-zinc-700 bg-gray-100 dark:bg-zinc-800 hover:shadow">
+    <div className="flex flex-col sm:w-3/6 pt-2 mt-2">
+      <div className="items-left sm:m-4 border rounded-lg border-gray-200 dark:border-zinc-700 bg-gray-100 dark:bg-zinc-800 hover:shadow">
         <div className="flex flex-row m-4">
           <div className="flex flex-col m-2"> 
             <p className="text-gray-800 dark:text-gray-200 font-medium">
