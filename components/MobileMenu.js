4ca7165cb6d85ee415f58db86af3bef27e1c8d77
Michael Yang <chengzhou.y@gmail.com>
2021-12-25 11:10:39 +0100

    Fix mobile menu delay

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -67,7 +67,7 @@ export default function MobileMenu() {
           </li>
           <li
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
-            style={{ transitionDelay: '175ms' }}
+            style={{ transitionDelay: '200ms' }}
           >
             <Link href="/contact">
               <a className="flex w-auto pb-4">Contact</a>
